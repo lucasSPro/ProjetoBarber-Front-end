af56cebc9bdeab1a10f116248462da012991ccb5
lucasSPro <lucas.san.77@gmail.com>
2020-08-10 15:47:22 -0300

    add content lis dashboard

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContainer, Profile } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContainer,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Section,
+  Appointment,
+  Calendar,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   const { signOut, user } = useAuth();
   return (
     <Container>
@@ -25,6 +38,77 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContainer>
       </Header>
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 10</span>
+            <span>Segunda-feira</span>
+          </p>
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars1.githubusercontent.com/u/31446385?s=460&u=4b84757571731e9a82d85b6c9d88a1d7e8ff0f3f&v=4"
+                alt="lucas dos Santos"
+              />
+
+              <strong>Lucas dos Santos</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+          <Section>
+            <strong>Manhã</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars1.githubusercontent.com/u/31446385?s=460&u=4b84757571731e9a82d85b6c9d88a1d7e8ff0f3f&v=4"
+                  alt="lucas dos Santos"
+                />
+                <strong>Lucas dos Santos</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars1.githubusercontent.com/u/31446385?s=460&u=4b84757571731e9a82d85b6c9d88a1d7e8ff0f3f&v=4"
+                  alt="lucas dos Santos"
+                />
+                <strong>Lucas dos Santos</strong>
+              </div>
+            </Appointment>
+          </Section>
+          <Section>
+            <strong>Tarde</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars1.githubusercontent.com/u/31446385?s=460&u=4b84757571731e9a82d85b6c9d88a1d7e8ff0f3f&v=4"
+                  alt="lucas dos Santos"
+                />
+                <strong>Lucas dos Santos</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
